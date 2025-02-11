4918322b40e4ecdd590b93207960e54f78a8cdb2
InkaraT-SE18 <157287894+InkaraT-SE18@users.noreply.github.com>
2025-02-11 11:19:23 +0500

    Update server.js

--- server.js
+++ server.js
@@ -8,19 +8,31 @@ const cors = require('cors');
 const app = express();
 const morgan = require("morgan");
 require('dotenv').config();
-app.use(morgan('dev'));
+
+
 app.use(cors({
-  origin: 'http://localhost:3000',  
+  origin: process.env.CLIENT_URL || '*', 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
-app.use(express.json()); 
+
+app.use(morgan('dev'));
+app.use(express.json());
+
 app.use('/api/products', productRoutes);
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use('/api/orders', ordersRouter);
 app.use("/api/payments", require("./routes/paymentRoute"));
-app.use(express.static(path.join(__dirname, 'public')));
-const PORT = process.env.PORT;
+
+
+app.use(express.static(path.join(__dirname, "public"))); 
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Сервер работает на http://localhost:${PORT}`);
 });
